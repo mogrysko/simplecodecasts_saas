bc90193442cbf66e4a0b983cf4b6e003c0bb381f
Mike Ogrysko <michael.ogrysko@comcast.net>
2015-06-03 00:14:34 +0000

    added profile controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get '/about' => 'pages#about'
   root 'pages#home'
   resources :contacts
+  resources :users do
+    resource :profile
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
