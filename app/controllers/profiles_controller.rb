3c306631cedb5403dc6da0d48c4b5dba36178076
Mike Ogrysko <michael.ogrysko@comcast.net>
2015-06-04 23:12:02 +0000

    added edit form profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -13,7 +13,13 @@ class ProfilesController < ApplicationController
       render action: :new
     end
   end
-  
+  def edit
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+  end
+  def update
+    
+  end
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
