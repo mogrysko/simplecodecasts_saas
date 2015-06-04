b4a0604aecbe0c275284ad85f248b629f1488eff
Mike Ogrysko <michael.ogrysko@comcast.net>
2015-06-04 01:44:58 +0000

    changed new action in profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,7 @@
 class ProfilesController < ApplicationController
   def new
     @user = User.find(params[:user_id])
-    @variable = params[:hello]
-    @profile = @user.build_profile
+    @profile = Profile.new
   end
   def create
     @user = User.find(params[:user_id])
